01f8af12339bbbcca3e7ff7398cbfabc0db3ea60
Bora KIŞ <izzetkis@hotmail.com>
2021-08-03 14:38:57 +0300

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,11 @@
+import random
+
+dice_rolls = 2
+for i in range(0,dice_rolls):
+
 def main():
-  print('You rolled a die')
+  roll = random.randint(1,6)
+  print(f'You rolled a {roll}')
 
 if __name__== "__main__":
   main()
